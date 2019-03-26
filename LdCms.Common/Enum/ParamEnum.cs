c8670910d17cdfebeb5fae3a3cdb5ca8f348ddcf
草 小 <6666298@163.com>
2019-03-26 12:14:00 +0800

    创建项目

---
+++ LdCms.Common/Enum/ParamEnum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LdCms.Common.Enum
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ParamEnum
+    {
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public enum Sex
+        {
+            保密=0,
+            男=1,
+            女=2
+        }
+
+
+    }
+}
---
+++ LdCms.Web/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+namespace LdCms.Web.Controllers
+{
+    using LdCms.IBLL.Sys;
+    using LdCms.Web.Models;
+    
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class HomeController : Controller
+    {
+        private readonly SiteConfig SiteConfig;
+        private readonly IHttpContextAccessor Accessor;
+        private readonly IConfigService ConfigService;
+        
+        public HomeController(IOptions<SiteConfig> SiteConfig, IHttpContextAccessor Accessor, IConfigService ConfigService)
+        {
+            this.SiteConfig = SiteConfig.Value;
+            this.Accessor = Accessor;
+            this.ConfigService = ConfigService;
+        }
+        public IActionResult Index()
+        {
+            try
+            {
+                int systemId = BaseSystemConfig.SystemID;
+                string companyId = SiteConfig.CompanyID;
+                var entity = ConfigService.GetConfig(systemId, companyId);
+                string homeUrl =string.IsNullOrEmpty(entity.HomeUrl)? SiteConfig.HomeUrl : entity.HomeUrl;
+                return Redirect(homeUrl);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public IActionResult Error()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                @ViewBag.Error = ex.Message;
+                return View();
+            }
+        }
+        public JsonResult Show()
+        {
+            return Json(SiteConfig);
+        }
+
+    }
+
+
+}
